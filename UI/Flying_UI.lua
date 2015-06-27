f04010adbe0234ce5b8bdcb0dd33027c8b9c37a3
Lynx3d <lynx3d@some-imaginary-isp.org>
2015-06-28 01:23:01 +0200

    Convert Flying_UI to generic window code

--- UI/Flying_UI.lua
+++ UI/Flying_UI.lua
@@ -1,82 +1,9 @@
 ﻿Dta.flying_ui = {}
 
-function Dta.flying_ui.createWindowFlying(name, parent, title, width, height, x, y, closable, movable, closeCallback, moveCallback)
-	local windowFlying = UI.CreateFrame("Frame", name, parent)
-	windowFlying:SetHeight(height)
-	windowFlying:SetWidth(width)
-	windowFlying:SetPoint("TOPLEFT", UIParent, "TOPLEFT", x, y)
-	-- windowFlying:SetBackgroundColor(1, 1, 1, 0.5) --Debug
-	windowFlying:SetLayer(30)
-
-	windowFlying.background = UI.CreateFrame("Texture", name .. "Background", windowFlying)
-	windowFlying.background:SetPoint("TOPLEFT", windowFlying, "TOPLEFT", 0, 0)
-	windowFlying.background:SetPoint("BOTTOMRIGHT", windowFlying, "TOPRIGHT", 0, height)
-	windowFlying.background:SetTexture("Rift", "dimension_window_bg_right_large.png.dds")
-	windowFlying.background:SetLayer(3)
-
-	local background2Position = windowFlying:GetHeight()-80
-	windowFlying.background2 = UI.CreateFrame("Texture", name .. "Background2", windowFlying)
-	windowFlying.background2:SetPoint("CENTERX", windowFlying, "CENTERX")
-	windowFlying.background2:SetPoint("CENTERY", windowFlying, "CENTERY", nil, background2Position/2)
-	windowFlying.background2:SetWidth(width)
-	windowFlying.background2:SetHeight(95)
-	windowFlying.background2:SetAlpha(0.3)
-	windowFlying.background2:SetTexture("Rift", "dimensions_tools_header.png.dds")
-	windowFlying.background2:SetLayer(5)
-
-
-	if closable then
-		windowFlying.closeBtn = Dta.ui.createButton(name .. "CloseBtn", windowFlying, windowFlying:GetWidth()-35, -33,0, 0, nil, "close", closeCallback)
-		windowFlying.closeBtn:SetLayer(4)
-	end
-
-	if movable then
-		windowFlying.moveFrame = UI.CreateFrame("Texture", name .. "WindowMover", windowFlying)
-		windowFlying.moveFrame:SetPoint("TOPLEFT", windowFlying, "TOPLEFT", 0, -40)
-		windowFlying.moveFrame:SetPoint("BOTTOMRIGHT", windowFlying, "TOPRIGHT", 0, 5)
-		windowFlying.moveFrame:SetTexture("Rift", "dimensions_main_bg_top.png.dds")
-		windowFlying.moveFrame:SetLayer(2)
-		--windowFlying.moveFrame:SetBackgroundColor(1, 0, 0, 0.5) --Debug
-
-		windowFlying.header = UI.CreateFrame("Text", name .. "header", windowFlying.moveFrame)
-		windowFlying.header:SetFontSize(20)
-		windowFlying.header:SetText(title)
-		windowFlying.header:SetFontColor(0,0,0,1)
-		windowFlying.header:SetPoint("CENTERX", windowFlying.moveFrame, "CENTERX")
-		windowFlying.header:SetPoint("CENTERY", windowFlying.moveFrame, "CENTERY", nil, 5)
-
-		local dragging = false
-
-		windowFlying.moveFrame:EventAttach(Event.UI.Input.Mouse.Left.Down, function(self, h)
-			dragging = true
-			mouse = Inspect.Mouse()
-			dragStartX = mouse.x - windowFlying:GetLeft()
-			dragStartY = mouse.y - windowFlying:GetTop()
-		end, "LMouseDown")
-
-		windowFlying.moveFrame:EventAttach(Event.UI.Input.Mouse.Left.Up, function(self, h)
-			dragging = false
-			if moveCallback ~= nil then moveCallback() end
-		end, "LMouseUp")
-
-		windowFlying.moveFrame:EventAttach(Event.UI.Input.Mouse.Cursor.Move, function(self,h)
-			local x, y
-			local md = Inspect.Mouse()
-			x = md.x
-			y = md.y
-			if dragging then
-				windowFlying:SetPoint("TOPLEFT", UIParent, "TOPLEFT", x - dragStartX, y - dragStartY)
-				end
-		end, "MouseMove")
-
-		return windowFlying
-	end
-end
-
 local FlyingWindowSettings = {
 	TITLE = Lang[Dta.Language].Titles.TribalMagic,
 	WIDTH = 300,
-	HEIGHT = 270,
+	HEIGHT = 265,
 	CLOSABLE = true,
 	MOVABLE = true,
 }
@@ -84,7 +11,7 @@ local FlyingWindowSettings = {
 function Dta.flying_ui.buildFlyingWindow()
 	local x = Dta.settings.get("FlyingwindowPosX")
 	local y = Dta.settings.get("FlyingwindowPosY")
-	Flyingwindow = Dta.flying_ui.createWindowFlying("FlyingWindow",
+	local newWindow = Dta.ui.Window.Create("FlyingWindow",
 							Dta.ui.context,
 							FlyingWindowSettings.TITLE,
 							FlyingWindowSettings.WIDTH,
@@ -96,11 +23,20 @@ function Dta.flying_ui.buildFlyingWindow()
 							Dta.flying_ui.FlyingWindowClosed,
 							Dta.flying_ui.FlyingWindowMoved
 							)
+	local Flyingwindow = newWindow.content
+
+	Flyingwindow.background2 = UI.CreateFrame("Texture", "FlyingwindowBackground2", Flyingwindow)
+	Flyingwindow.background2:SetPoint("BOTTOMCENTER", Flyingwindow, "BOTTOMCENTER")
+	Flyingwindow.background2:SetWidth(FlyingWindowSettings.WIDTH)
+	Flyingwindow.background2:SetHeight(80)
+	Flyingwindow.background2:SetAlpha(0.3)
+	Flyingwindow.background2:SetTexture("Rift", "dimensions_tools_header.png.dds")
+	Flyingwindow.background2:SetLayer(5)
 
 	-------------------------------
 	--ITEM DETAILS
 	-------------------------------
-	Flyingwindow.DFlying = Dta.ui.createFrame("ImportExport", Flyingwindow, 10, 10, Flyingwindow:GetWidth()-20, Flyingwindow:GetHeight()-20)
+	Flyingwindow.DFlying = Dta.ui.createFrame("ImportExport", Flyingwindow, 10, 5, Flyingwindow:GetWidth()-20, Flyingwindow:GetHeight()-20)
 	Flyingwindow.DFlying:SetLayer(30)
 	--Flyingwindow.DFlying:SetBackgroundColor(1, 0, 0, 0.5) --Debug
 
@@ -138,7 +74,10 @@ function Dta.flying_ui.buildFlyingWindow()
 		Dta.pitchButtons[i].Event.LeftPress = function() Dta.flying.AdjustPitch(i) end
 	end
 
-	return Flyingwindow
+	-- TODO: temp fix for new window hierarchy
+	newWindow.DFlying = Flyingwindow.DFlying
+
+	return newWindow
 end
 
 -- Show the toolbox window
@@ -171,6 +110,6 @@ end
 
 --Called when the window has been moved
 function Dta.flying_ui.FlyingWindowMoved()
-	Dta.settings.set("FlyingwindowPosX", Flyingwindow:GetLeft())
-	Dta.settings.set("FlyingwindowPosY", Flyingwindow:GetTop())
+	Dta.settings.set("FlyingwindowPosX", Dta.ui.windowFlying:GetLeft())
+	Dta.settings.set("FlyingwindowPosY", Dta.ui.windowFlying:GetTop())
 end
